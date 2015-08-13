d60578ed48d710ce8a5851fce87c677af29f8848
marcianne <oday.marcianne@me.com>
2015-08-13 11:34:04 -0400

    Plugin Boilerplate
    via wppb.io

---
+++ fbeventslist/includes/class-fbeventslist-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.marcianneoday.com
+ * @since      1.0.0
+ *
+ * @package    Fbeventslist
+ * @subpackage Fbeventslist/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Fbeventslist
+ * @subpackage Fbeventslist/includes
+ * @author     Marcianne O'Day <oday.marcianne@me.com>
+ */
+class Fbeventslist_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
