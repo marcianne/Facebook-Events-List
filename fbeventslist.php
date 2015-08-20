c1fed106fab02dabe311c67430364bb4e7b119f4
marcianne <marcianne@users.noreply.github.com>
2015-08-19 21:44:39 -0400

    update fbeventslist.php

--- fbeventslist.php
+++ fbeventslist.php
@@ -7,15 +7,13 @@
  * Plugin Name:       Facebook Events List
  * Plugin URI:        https://github.com/marcianne/Facebook-Events-List
  * Description:       An unbranded, minimally-styled list of facebook events with options page.
- * Version:           1.0.2
+ * Version:           1.0.4
  * Author:            Marcianne O'Day
  * Author URI:        http://www.marcianneoday.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       fbeventslist
  * Domain Path:       /languages
-*  GitHub Plugin URI: https://github.com/marcianne/Facebook-Events-List
-*  GitHub Branch:     master
  */
  // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -32,10 +30,25 @@ function fbel_load_dependencies() {
 if ( ! class_exists( 'CMB2_Bootstrap_210', false ) ) {
 require_once( FBEL_PLUGIN_PATH . "wp-content/plugins/cmb2/init.php");
 }
-// load github updater from composer plugin dir if it's not already loaded
-if ( ! class_exists( 'WPUpdatePhp' ) ) {
-// require_once ( FBEL_PLUGIN_PATH . '/wp-content/plugins/github-updater/github-updater.php' );
-}
+// load and instantiate github updater 
+	include_once(FBEL_PLUGIN_PATH . 'updater.php');
+    if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+        $config = array(
+            'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+            'proper_folder_name' => 'Facebook-Events-List', // this is the name of the folder your plugin lives in
+            'api_url' => 'https://api.github.com/repos/marcianne/Facebook-Events-List', // the GitHub API url of your GitHub repo
+            'raw_url' => 'https://raw.github.com/marcianne/Facebook-Events-List/master', // the GitHub raw url of your GitHub repo
+            'github_url' => 'https://github.com/marcianne/Facebook-Events-List', // the GitHub url of your GitHub repo
+            'zip_url' => 'https://github.com/marcianne/Facebook-Events-List/zipball/master', // the zip url of the GitHub repo
+            'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+            'requires' => '3.0', // which version of WordPress does your plugin require?
+            'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+            'readme' => 'README.md' // which file to use as the readme for the version number
+          //  'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+        );
+        new WP_GitHub_Updater($config);
+    }
+
 // if it's not already loaded by another plugin, add mustardbees cmb-field-select2 https://github.com/mustardBees/cmb-field-select2
 if (! function_exists('pw_select2_enqueue')){ // pw_select2_enqueue() is just an arbitrary funciton in cmb-field-select2
 require_once( FBEL_PLUGIN_PATH . '/cmb-field-select2/cmb-field-select2.php');
@@ -521,7 +534,3 @@ function fbel_options_enqueue($hook) {
 }
 
 }
-
-
-
-
