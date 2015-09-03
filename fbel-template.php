3d06b1ec328acdca7833738408eb50af37ae750a
marcianne <marcianne@users.noreply.github.com>
2015-09-02 23:37:50 -0400

    Update fbel-template.php

--- fbel-template.php
+++ fbel-template.php
@@ -1,7 +1,7 @@
 <div class="single_event_container"><!-- div containing each single event -->	
-<figure class="event_pic"><a href="<?php echo $event_url; ?>"> <!-- event pic container -->
+<div class="event_pic"><a href="<?php echo $event_url; ?>"> <!-- event pic container -->
 <img src="<?php echo $pic_sm; ?>"/></a>
-</figure> <!-- // event pic container -->
+</div> <!-- // event pic container -->
 
 <h3 class="event_name"><a href="<?php echo $event_url; ?>"><?php echo($name); ?></a></h3>  <!-- event name with url link to fb page -->
 <h4 class="event_start"><?php echo $start_date ?></h4>  <!-- event start date-->
